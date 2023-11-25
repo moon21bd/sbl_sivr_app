7441287b0fa587fab41bee638f335426d39b308b
Raqibul Hasan <raqibul.hasan@ssd-tech.io>
2023-11-25 23:17:22 +0600

    debugging

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2567,7 +2567,7 @@ class ApiController extends ResponseController
 
         $loginData = config('api.crm_ticket_login_info');
         $response = $apiHandler->postCall($url, $loginData);
-        Log::info('CRM Login response : ' . json_encode($response['data']));
+        // Log::info('CRM Login response : ' . json_encode($response['data']));
 
         /*{
             "success": true,
@@ -2604,7 +2604,7 @@ class ApiController extends ResponseController
 
         if ($response['status'] === 'success' && $response['statusCode'] === Response::HTTP_OK) {
             $data = json_decode($response['data'], true);
-            Log::info('CRM Login Token : ' . json_encode(['res' => $data['token']]));
+
             if ($data['success'] && !empty($data['data']['token'])) {
                 Cache::put('crm_access_token', $data['data']['token'], now()->addMinutes(120));
 
