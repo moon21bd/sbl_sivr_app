6ab748ca52f68c7905c7239115a3eb94fadda233
Raqibul Hasan <raqibul.hasan@ssd-tech.io>
2023-12-10 22:30:21 +0600

    new migration file for response added

---
+++ database/migrations/2023_12_10_222252_alter_response_column_of_api_logs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('api_logs', function (Blueprint $table) {
+            $table->longText('response')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('api_logs', function (Blueprint $table) {
+            $table->text('response')->change();
+        });
+    }
+};
