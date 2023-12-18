e7fb1510e699ef9eca3eee620346fc9ecf4a0031
Raqibul Hasan <raqibul.hasan@ssd-tech.io>
2023-12-18 23:18:03 +0600

    css issue

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -260,7 +260,7 @@
         }
 
         #swal2-html-container {
-            display: flex
+            display: flex;
             align-items: center;
             justify-content: center;
             flex-direction: column;
