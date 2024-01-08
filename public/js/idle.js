60e5d0f0d5e23ca45f910b65e8e537f6c6da0572
Raqibul Hasan <raqibul.hasan@ssd-tech.io>
2024-01-08 23:57:20 +0600

    logout on close has been implemented. also if user inactive for 10 mins and idle session will expire logic implemented.

--- public/js/idle.js
+++ public/js/idle.js
@@ -10,14 +10,12 @@ window.addEventListener('beforeunload', function (event) {
     }
 });
 
-// Show prompt on unload
 window.addEventListener('unload', function (event) {
     if (!isPageRefreshed) {
         // Page is being closed, show a prompt
         expireSession(isPageRefreshed);
     }
 });
-
 
 /*
 // WORKS ONLY FOR TAB CLOSE. NOT TRIGGERED IF PAGE RELOADED OR BEING REFRESHED.
@@ -31,59 +29,6 @@ window.addEventListener('unload', function() {
         .then(response => console.log('HTTP request successful:', response.data))
         .catch(error => console.error('Error making HTTP request:', error));
 });*/
-
-
-/*$(window).on("beforeunload", function () {
-    axios.post('/exitintent', {
-        isReloaded: 200,
-        exitIntentActive: exitIntentActive,
-    })
-        .then(response => {
-            // Handle the response if needed
-            console.log('HTTP request successful:', response.data);
-        })
-        .catch(error => {
-            // Handle errors
-            console.error('Error making HTTP request:', error);
-        });
-});*/
-
-/*let exitIntentActive = false;
-
-function handleExitIntent() {
-    if (!exitIntentActive) {
-        exitIntentActive = true;
-
-        // Check if the page is being reloaded or refreshed
-        if (performance.navigation.type === performance.navigation.TYPE_RELOAD ||
-            performance.navigation.type === performance.navigation.TYPE_NAVIGATE) {
-            alert('hi');
-            // Page is being refreshed or reloaded, don't capture the event
-            return;
-        }
-
-        console.log('Exit Intent Detected!');
-
-        // Perform your exit intent actions here
-        axios.post('/exitintent', {
-            isReloaded: 200,
-            exitIntentActive: exitIntentActive,
-            purpose: 'EXIT',
-        })
-            .then(response => {
-                // Handle the response if needed
-                console.log('HTTP request successful:', response.data);
-            })
-            .catch(error => {
-                // Handle errors
-                console.error('Error making HTTP request:', error);
-            });
-    }
-}
-
-// Detect tab close or browser navigation
-// window.addEventListener('beforeunload', handleExitIntent);*/
-
 
 // detect is user idle start from here.
 let idleTimer;
