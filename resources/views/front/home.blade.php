4db54a4b8675986797e88a8a194851fe25c80e96
Raqibul Hasan <raqibul.hasan@ssd-tech.io>
2024-01-10 12:57:31 +0600

    spg click on new tab added.

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -9,7 +9,7 @@
                    class="flex flex-col gap-3 lg:gap-4 justify-center items-center bg-white rounded-md px-2 py-4 lg:px-4 lg:py-6 cursor-pointer">
 
                     <div
-                        class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
+                            class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
                         <img class="w-6 h-6 lg:w-8 lg:h-8" src="{{ asset('img/icon/e-wallet.svg') }}" alt="">
                     </div>
                     <h3 class="text-[color:var(--text-black)] [font-size:var(--font-size-box-sm)] lg:[font-size:var(--font-size-box)] font-bold">
@@ -22,7 +22,7 @@
                    class="flex flex-col gap-3 lg:gap-4 justify-center items-center bg-white rounded-md px-2 py-4 lg:px-4 lg:py-6 cursor-pointer">
 
                     <div
-                        class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
+                            class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
                         <img class="w-6 h-6 lg:w-8 lg:h-8" src="{{ asset('img/icon/e-wallet-disable.svg') }}" alt="">
                     </div>
                     <h3 class="text-[color:var(--text-black)] [font-size:var(--font-size-box-sm)] lg:[font-size:var(--font-size-box)] font-bold">
@@ -36,7 +36,7 @@
                data-text="{{ __('scripts.default-call-center-text') }}"
                class="flex flex-col gap-3 lg:gap-4 justify-center items-center bg-white rounded-md px-2 py-4 lg:px-4 lg:py-6 cursor-pointer">
                 <div
-                    class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
+                        class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
                     <img class="w-6 h-6 lg:w-8 lg:h-8" src="{{ asset('img/icon/e-sheba.svg') }}" alt="">
                 </div>
                 <h3 class="text-[color:var(--text-black)] [font-size:var(--font-size-box-sm)] lg:[font-size:var(--font-size-box)] font-bold">
@@ -45,12 +45,12 @@
         </div>
 
         <div class="col-span-4 z-10">
-            <a href="javascript:void(0)" id="btnSPG"
+            <a href="https://sbl.com.bd:7070/" target="_blank"
                data-voice="{{ app()->getLocale() === 'en' ? config('voices.defaultCallForHelp.voice.en') : config('voices.defaultCallForHelp.voice.bn') }}"
                data-text="{{ __('scripts.default-call-center-text') }}"
                class="flex flex-col gap-3 lg:gap-4 justify-center items-center bg-white rounded-md px-2 py-4 lg:px-4 lg:py-6 cursor-pointer">
                 <div
-                    class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
+                        class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
                     <img class="w-6 h-6 lg:w-8 lg:h-8" src="{{ asset('img/icon/spg.svg') }}" alt="">
                 </div>
                 <h3 class="text-[color:var(--text-black)] [font-size:var(--font-size-box-sm)] lg:[font-size:var(--font-size-box)] font-bold">
@@ -58,38 +58,38 @@
             </a>
         </div>
 
-            @if(Session::get('account_verification_status', false))
-                <div class="col-span-4 z-10">
-                    <a href="javascript:void(0)" id="btnCards"
-                       class="flex flex-col gap-3 lg:gap-4 justify-center items-center bg-white rounded-md px-2 py-4 lg:px-4 lg:py-6 cursor-pointer">
-                        <div
+        @if(Session::get('account_verification_status', false))
+            <div class="col-span-4 z-10">
+                <a href="javascript:void(0)" id="btnCards"
+                   class="flex flex-col gap-3 lg:gap-4 justify-center items-center bg-white rounded-md px-2 py-4 lg:px-4 lg:py-6 cursor-pointer">
+                    <div
                             class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
-                            <img class="w-6 h-6 lg:w-8 lg:h-8" src="{{ asset('img/icon/cards.svg') }}" alt="">
-                        </div>
-                        <h3 class="text-[color:var(--text-black)] [font-size:var(--font-size-box-sm)] lg:[font-size:var(--font-size-box)] font-bold">
-                            {{ __('messages.cards-btn') }}</h3>
-                    </a>
-                </div>
-            @else
-                <div class="col-span-4 z-10">
-                    <a href="javascript:void(0)" id="btnCardsDisable"
-                       class="flex flex-col gap-3 lg:gap-4 justify-center items-center bg-white rounded-md px-2 py-4 lg:px-4 lg:py-6 cursor-pointer">
-                        <div
+                        <img class="w-6 h-6 lg:w-8 lg:h-8" src="{{ asset('img/icon/cards.svg') }}" alt="">
+                    </div>
+                    <h3 class="text-[color:var(--text-black)] [font-size:var(--font-size-box-sm)] lg:[font-size:var(--font-size-box)] font-bold">
+                        {{ __('messages.cards-btn') }}</h3>
+                </a>
+            </div>
+        @else
+            <div class="col-span-4 z-10">
+                <a href="javascript:void(0)" id="btnCardsDisable"
+                   class="flex flex-col gap-3 lg:gap-4 justify-center items-center bg-white rounded-md px-2 py-4 lg:px-4 lg:py-6 cursor-pointer">
+                    <div
                             class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
-                            <img class="w-6 h-6 lg:w-8 lg:h-8" src="{{ asset('img/icon/cards-disable.svg') }}" alt="">
-                        </div>
-                        <h3 class="text-[color:var(--text-black)] [font-size:var(--font-size-box-sm)] lg:[font-size:var(--font-size-box)] font-bold">
-                            {{ __('messages.cards-btn') }}</h3>
-                    </a>
-                </div>
-            @endif
+                        <img class="w-6 h-6 lg:w-8 lg:h-8" src="{{ asset('img/icon/cards-disable.svg') }}" alt="">
+                    </div>
+                    <h3 class="text-[color:var(--text-black)] [font-size:var(--font-size-box-sm)] lg:[font-size:var(--font-size-box)] font-bold">
+                        {{ __('messages.cards-btn') }}</h3>
+                </a>
+            </div>
+        @endif
 
 
         <div class="col-span-4 z-10">
             <a href="javascript:void(0)" id="btnAccountAndLoan"
                class="flex flex-col gap-3 lg:gap-4 justify-center items-center bg-white rounded-md px-2 py-4 lg:px-4 lg:py-6 cursor-pointer">
                 <div
-                    class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
+                        class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
                     <img class="w-6 h-6 lg:w-8 lg:h-8" src="{{ asset('img/icon/acc-loan.svg') }}" alt="">
                 </div>
                 <h3 class="text-[color:var(--text-black)] [font-size:var(--font-size-box-sm)] lg:[font-size:var(--font-size-box)] font-bold">
@@ -101,7 +101,7 @@
             <a href="javascript:void(0)" id="btnIslamiBanking"
                class="flex flex-col gap-3 lg:gap-4 justify-center items-center bg-white rounded-md px-2 py-4 lg:px-4 lg:py-6 cursor-pointer">
                 <div
-                    class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
+                        class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
                     <img class="w-6 h-6 lg:w-8 lg:h-8" src="{{ asset('img/icon/islami-bank.svg') }}" alt="">
                 </div>
                 <h3 class="text-[color:var(--text-black)] [font-size:var(--font-size-box-sm)] lg:[font-size:var(--font-size-box)] font-bold">
@@ -115,7 +115,7 @@
                data-text="{{ __('scripts.default-call-center-text') }}"
                class="flex flex-col gap-3 lg:gap-4 justify-center items-center bg-white rounded-md px-2 py-4 lg:px-4 lg:py-6 cursor-pointer">
                 <div
-                    class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
+                        class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
                     <img class="w-6 h-6 lg:w-8 lg:h-8" src="{{ asset('img/icon/agent-bank.svg') }}" alt="">
                 </div>
                 <h3 class="text-[color:var(--text-black)] [font-size:var(--font-size-box-sm)] lg:[font-size:var(--font-size-box)] font-bold">
@@ -129,7 +129,7 @@
                data-text="{{ __('scripts.default-call-center-text') }}"
                class="flex flex-col gap-3 lg:gap-4 justify-center items-center bg-white rounded-md px-2 py-4 lg:px-4 lg:py-6 cursor-pointer">
                 <div
-                    class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
+                        class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
                     <img class="w-6 h-6 lg:w-8 lg:h-8" src="{{ asset('img/icon/bank-products.svg') }}" alt="">
                 </div>
                 <h3 class="text-[color:var(--text-black)] [font-size:var(--font-size-box-sm)] lg:[font-size:var(--font-size-box)] font-bold">
@@ -141,7 +141,7 @@
             <a href="javascript:void(0)" id="btnCreateIssue"
                class="flex flex-col gap-3 lg:gap-4 justify-center items-center bg-white rounded-md px-2 py-4 lg:px-4 lg:py-6 cursor-pointer">
                 <div
-                    class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
+                        class="w-10 h-10 lg:w-14 lg:h-14 flex justify-center items-center rounded-md bg-[color:var(--brand-color-blue)]">
                     <img class="w-6 h-6 lg:w-8 lg:h-8" src="{{ asset('img/icon/my-complaint.svg') }}" alt="">
                 </div>
                 <h3 class="text-[color:var(--text-black)] [font-size:var(--font-size-box-sm)] lg:[font-size:var(--font-size-box)] font-bold">
